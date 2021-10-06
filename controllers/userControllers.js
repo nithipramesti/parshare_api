2dda398fb6c591afccf3614fae0402c59bac8ec6
aldi278 <mohammad.mauliadi@gmail.com>
2021-10-06 12:50:05 +0700

    Update API untuk fitur Registrasi

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,7 +6,7 @@ module.exports = {
         let {email, username, password} = req.body
         console.log(username)
         password = Crypto.createHmac("sha1", process.env.SHARED_KEY).update(password).digest('hex') //hash password before save to db
-        checkUserQuery = `select * from users where username = "${username}"`
+        checkUserQuery = `select * from users where email = "${email}"`
         db.query(checkUserQuery,(checkErr,checkResult) => {
             if(checkErr){
                 return res.status(500).send({
@@ -29,10 +29,28 @@ module.exports = {
                             error : err,
                         })
                     }
-                    res.status(200).send({
-                        message : "Register Success",
-                        success : true
-                    })
+                    if(result.insertId){
+                        let selectQuery = `select * from users where id_user = ${result.insertId}`
+                        db.query(selectQuery, (errRes,resultRes) => {
+                            if(errRes){
+                                res.status(500).send("internal server error")
+                            } 
+                            
+                            let {id_user, username, email, password, role, verified, gender, fullname, address, birthdate, picture_link} = resultRes[0]
+
+                            res.status(200).send({
+                                message : `Hi ${username}, please check your email to complete your registration process`,
+                                success : true,
+                                data : {
+                                    id_user : resultRes[0].id_user,
+                                    email : resultRes[0].email,
+                                    username : resultRes[0].username,
+                                    role : resultRes[0].role,
+                                    isVerified : resultRes[0].verified
+                                }
+                            })
+                        })
+                    }
                 })
             }
             
