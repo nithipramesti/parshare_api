517f404a8a56de769e75a64afb6d1160778321e6
thecodesdev <ilhamaulana24@gmail.com>
2021-10-07 14:04:52 +0700

    BE-ILHAM-001 Memperbaiki delete product

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,17 +1,18 @@
 const { db } = require("../database");
+const { uploader } = require("../helper/uploader");
+const fs = require("fs");
 
 module.exports = {
-  deleteProduct: (req,res) => {
+  deleteProduct: (req, res) => {
     if(req.user.role === "admin"){
-      let {id} = req.body;
-      let addQuery = `update products set active = 'false' where id_product = '${id}'`
-      db.query(addQuery, (err, result) => {
+      let deleteQuery = `update products set active = 'false' where id_product = ${db.escape(req.query.id)}`
+      db.query(deleteQuery, (err, result) => {
         if(err){
           return res.status(500).send({
             success: false,
             data: err
           })
-        }else if(result.insertId){
+        }else{
           return res.status(200).send({
             success: true,
             data: "Delete succeed!"
