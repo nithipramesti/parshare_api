06242e31eb05fdb47053845874d89f5dec879f68
nithipramesti <nithipramesti1508@gmail.com>
2021-10-09 11:00:20 +0800

    BE-Nithi-005: Forgot password finishing

--- controllers/changePasswordControllers.js
+++ controllers/changePasswordControllers.js
@@ -1,6 +1,8 @@
 const { db } = require("../database");
 const { createToken } = require("../helper/createToken");
 const Crypto = require("crypto");
+const transporter = require("../helper/nodemailer");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   sendEmail: (req, res) => {
@@ -46,20 +48,44 @@ module.exports = {
         });
 
         if (verified != "true") {
-          res.status(200).send({ message: "Your account is not verified" });
+          res.status(200).send({
+            message:
+              "Your account is not verified, please verify your account first",
+          });
         } else {
+          //SEND EMAIL
+          let mail = {
+            from: "Admin Parshare <parshare.company@gmail.com>",
+            to: `${email}`,
+            subject: "Account Verification",
+            html: `Hi ${username},
+                        <br>
+                        <br>
+                        Please click the link below to reset your password
+                        <br>
+                        <a href = "http://localhost:3000/reset-password/${token}">Reset Password</a>
+                        <br>
+                        `,
+          };
+
+          transporter.sendMail(mail, (errMail, resMail) => {
+            if (errMail) {
+              console.log(errMail);
+              res.status(200).send({
+                errMessage: "Sending reset password failed, please try again",
+                success: false,
+              });
+            }
+
+            res.status(200).send({
+              message:
+                "Link sent successfully. Please check your email and follow the instructions to reset your password.",
+            });
+          });
+
           console.log(
             `Email '${email}' is registered, will send reset password link`
           );
-
-          //SEND EMAIL HERE////////
-
-          res.status(200).send({
-            dataLogin: results[0],
-            token,
-            message:
-              "Link sent successfully. Please check your email and follow the instructions to reset your password.",
-          });
         }
       } else {
         res.status(200).send({
@@ -67,7 +93,113 @@ module.exports = {
         });
       }
     });
+  },
 
-    //send email
+  decodeToken: (req, res) => {
+    //Decode token
+    jwt.verify(req.token, `${process.env.SHARED_KEY}`, (err, decode) => {
+      if (err) {
+        return res.status(401).send({ errMessage: "Can't decode token" });
+      }
+      req.dataDecode = decode; //decrypt token back into user data
+    });
+
+    //MySQL query to get data from user email
+    let scriptQuery = `Select * from users where email=${db.escape(
+      req.dataDecode.email
+    )};`;
+
+    //Check & get user data from MySQL
+    db.query(scriptQuery, (err, results) => {
+      if (err) {
+        res.status(500).send({ errMessage: "Internal server error" });
+      }
+
+      if (results[0]) {
+        console.log(
+          `Token decoded and match user data with username ${results[0].username}`
+        );
+
+        res
+          .status(200)
+          .send({ userData: results[0], message: "Token is valid" });
+      }
+    });
+  },
+
+  resetPassword: (req, res) => {
+    console.log(
+      `Will change password of ${req.body.userData.username} to '${req.body.newPassword}'`
+    );
+
+    //Hashing password
+    req.body.newPassword = Crypto.createHmac(
+      "sha1",
+      `${process.env.SHARED_KEY}`
+    )
+      .update(req.body.newPassword)
+      .digest("hex");
+
+    //CHANGE USER PASSWORD
+    let updateQuery = `Update users set password = ${db.escape(
+      req.body.newPassword
+    )} where email=${db.escape(req.body.userData.email)};`;
+
+    db.query(updateQuery, (err, results) => {
+      if (err) res.status(500).send({ errMessage: "Update user data failed" });
+
+      //GET UPDATED USER DATA
+      let selectQuery = `Select * from users where email=${db.escape(
+        req.body.userData.email
+      )};`;
+
+      db.query(selectQuery, (err, results) => {
+        if (err) res.status(500).send({ errMessage: "Get user data failed" });
+
+        if (results[0]) {
+          //Destructuring results
+          let {
+            id_user,
+            username,
+            email,
+            password,
+            role,
+            verified,
+            gender,
+            fullname,
+            address,
+            birthdate,
+            pitcure_link,
+          } = results[0];
+
+          //create TOKEN -- will save on local storage via FE
+          let token = createToken({
+            id_user,
+            username,
+            email,
+            password,
+            role,
+            verified,
+            gender,
+            fullname,
+            address,
+            birthdate,
+            pitcure_link,
+          });
+
+          console.log("Change password success, username: ", username);
+
+          res.status(200).send({
+            dataLogin: results[0],
+            token,
+            message: "Change password success",
+          });
+        } else {
+          res.status(200).send({
+            errMessage: "Can't match user data",
+          });
+        }
+      });
+    });
   },
 };
