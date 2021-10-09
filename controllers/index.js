83ba7e5014d774dc213bbaad68f47cbfdf4951c4
thecodesdev <ilhamaulana24@gmail.com>
2021-10-09 07:35:03 +0700

    BE-ILHAM-001 Memperbaiki delete product

--- controllers/index.js
+++ controllers/index.js
@@ -5,13 +5,9 @@ const transactionControllers = require("./transactionControllers");
 const cartControllers = require("./cartControllers");
 
 module.exports = {
-<<<<<<< HEAD
-  productControllers,
-=======
   userControllers,
   parcelControllers,
   productControllers,
   cartControllers,
   transactionControllers,
->>>>>>> 5eead091ecc25c612e1966fc5d7ee04a925b544b
 };
