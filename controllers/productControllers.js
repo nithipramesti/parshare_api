0425c7e51884f5f284fa9d326f2cc0daa8ea066c
Abdi-01 <abdialghi@gmail.com>
2021-10-12 13:30:55 +0700

    BE-master-001 : fix bug multer

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -6,12 +6,12 @@ module.exports = {
   listProduct: (req, res) => {
     let getQuery = `SELECT id_product as id, id_product, product_name, product_price, image_product, db_parshare.products.id_category, db_parshare.categories.category, product_quantity, db_parshare.products.active FROM products INNER JOIN db_parshare.categories ON db_parshare.products.id_category = db_parshare.categories.id_category`
     db.query(getQuery, (err, result) => {
-      if(err){
+      if (err) {
         return res.status(500).send({
           success: false,
           data: err
         })
-      }else{
+      } else {
         return res.status(200).send({
           success: true,
           data: result
@@ -20,22 +20,22 @@ module.exports = {
     })
   },
   getProduct: (req, res) => {
-    if(req.query.id) {
+    if (req.query.id) {
       let getQuery = `SELECT id_product as id, id_product, product_name, product_price, image_product, db_parshare.products.id_category, db_parshare.categories.category, product_quantity, db_parshare.products.active FROM products INNER JOIN db_parshare.categories ON db_parshare.products.id_category = db_parshare.categories.id_category WHERE id_product = ${req.query.id}`
       db.query(getQuery, (err, result) => {
-        if(err){
+        if (err) {
           return res.status(500).send({
             success: false,
             data: err
           })
-        }else{
+        } else {
           return res.status(200).send({
             success: true,
             data: result[0]
           })
         }
       })
-    }else{
+    } else {
       res.status(500).status({
         success: false,
         data: "Missing query!"
@@ -43,65 +43,67 @@ module.exports = {
     }
   },
   addProduct: (req, res) => {
-    if(req.user.role === 'admin'){
-      let { name, price, category, quantity } = JSON.parse(req.body.data);
-      if(name && price && category && quantity && req.files) {
+    if (req.user.role === 'admin') {
+      // if(name && price && category && quantity && req.files) {
         try {
           let path = '/images'
           const upload = uploader(path, 'IMG').fields([{ name: 'file' }])
           
           upload(req, res, (error) => {
             if (error) {
-                console.log(error)
-                res.status(500).send(error)
+              console.log(error)
+              res.status(500).send(error)
             }
             
             const { file } = req.files
             const filepath = file ? path + '/' + file[0].filename : null
             let data = JSON.parse(req.body.data)
             data.image = filepath
+            let { name, price, category, quantity } = data
+            console.table(file)
+            console.table(data)
             
-            let addQuery = `insert into products values (null, ${db.escape(name)}, ${db.escape(filepath)}, ${db.escape(price)}, ${db.escape(category)}, ${db.escape(quantity)}, 'true')`
-            db.query(addQuery, (err, result) => {
-              if(err){
-                fs.unlinkSync('./public' + filepath)
-                return res.status(500).send({
-                  success: false,
-                  data: err
-                })
-              }else if(result.insertId){
-                let getQuery = `select * from products where id_product='${result.insertId}'`
-                db.query(getQuery, (err2, result2) => {
-                  if(err2){
-                    fs.unlinkSync('./public' + filepath)
-                    return res.status(500).send({
-                      success: false,
-                      data: err2
-                    })
-                  }else{
-                    return res.status(200).send({
-                      success: true,
-                      data: result2[0]
-                    })
-                  }
-                })
-              }
-            })
+          let addQuery = `insert into products values (null, ${db.escape(name)}, ${db.escape(filepath)}, ${db.escape(price)}, ${db.escape(category)}, ${db.escape(quantity)}, 'true')`
+          db.query(addQuery, (err, result) => {
+            if(err){
+              fs.unlinkSync('./public' + filepath)
+              return res.status(500).send({
+                success: false,
+                data: err
+              })
+            }else if(result.insertId){
+              let getQuery = `select * from products where id_product='${result.insertId}'`
+              db.query(getQuery, (err2, result2) => {
+                if(err2){
+                  fs.unlinkSync('./public' + filepath)
+                  return res.status(500).send({
+                    success: false,
+                    data: err2
+                  })
+                }else{
+                  return res.status(200).send({
+                    success: true,
+                    data: result2[0]
+                  })
+                }
+              })
+            }
           })
-        } catch (error) {
-          console.log(error)
-          res.status(500).send({
-            success: false,
-            data: error
-          })
-        }
-      }else{
+        })
+      } catch (error) {
+        console.log(error)
         res.status(500).send({
           success: false,
-          data: "Missing query!"
+          data: error
         })
       }
-    }else{
+      // }else{
+      //   res.status(500).send({
+      //     success: false,
+      //     data: "Missing query!"
+      //   })
+      // }
+    } else {
       return res.status(500).send({
         success: false,
         data: "User not allowed!"
@@ -109,20 +111,20 @@ module.exports = {
     }
   },
   editProduct: (req, res) => {
-    if(req.user.role === "admin"){
+    if (req.user.role === "admin") {
       console.log(req)
       let { id, name, price, category, quantity } = JSON.parse(req.body.data);
-      if(id && name && category && quantity && req.files) {
+      if (id && name && category && quantity && req.files) {
         try {
           let path = '/images'
           const upload = uploader(path, 'IMG').fields([{ name: 'file' }])
-          
+
           upload(req, res, (error) => {
             if (error) {
-                console.log(error)
-                res.status(500).send(error)
+              console.log(error)
+              res.status(500).send(error)
             }
-            
+
             const { file } = req.files
             const filepath = file ? path + '/' + file[0].filename : null
             let data = JSON.parse(req.body.data)
@@ -131,13 +133,13 @@ module.exports = {
             fs.unlinkSync('./public' + image);
             let updateQuery = `update products set product_name = ${db.escape(name)}, product_price = ${db.escape(price)}, image_product = ${db.escape(filepath)}, id_category = ${db.escape(category)}, product_quantity = ${db.escape(quantity)} where id_product = ${db.escape(id)}`
             db.query(updateQuery, (err2, result2) => {
-              if(err2){
+              if (err2) {
                 fs.unlinkSync('./public' + filepath)
                 return res.status(500).send({
                   success: false,
                   data: err2
                 })
-              }else{
+              } else {
                 res.status(200).send({
                   status: true
                 })
@@ -151,23 +153,23 @@ module.exports = {
             data: error
           })
         }
-      }else if(id && name && category && quantity){
+      } else if (id && name && category && quantity) {
         let updateQuery = `update products set product_name = ${db.escape(name)}, product_price = ${db.escape(price)}, id_category = ${db.escape(category)}, product_quantity = ${db.escape(quantity)} where id_product = ${db.escape(id)}`
         db.query(updateQuery, (err, result) => {
-          if(err){
+          if (err) {
             return res.status(500).send({
               success: false,
               data: err
             })
-          }else{
+          } else {
             let getQuery = `select * from products where id_product = ${db.escape(id)}`
             db.query(getQuery, (err2, result2) => {
-              if(err2){
+              if (err2) {
                 return res.status(500).send({
                   success: false,
                   data: err2
                 })
-              }else{
+              } else {
                 return res.status(200).send({
                   success: true,
                   data: result2[0]
@@ -177,7 +179,7 @@ module.exports = {
           }
         })
       }
-    }else{
+    } else {
       return res.status(500).send({
         success: false,
         data: "User not allowed!"
@@ -185,29 +187,29 @@ module.exports = {
     }
   },
   deleteProduct: (req, res) => {
-    if(req.user.role === "admin"){
-      if(req.query.id){
+    if (req.user.role === "admin") {
+      if (req.query.id) {
         let deleteQuery = `update products set active = 'false' where id_product = ${db.escape(req.query.id)}`
         db.query(deleteQuery, (err, result) => {
-          if(err){
+          if (err) {
             return res.status(500).send({
               success: false,
               data: err
             })
-          }else{
+          } else {
             return res.status(200).send({
               success: true,
               data: "Delete succeed!"
             })
           }
         })
-      }else{
+      } else {
         return res.status(500).send({
           success: false,
           data: "Missing query!"
         })
       }
-    }else{
+    } else {
       return res.status(500).send({
         success: false,
         data: "User not allowed!"
