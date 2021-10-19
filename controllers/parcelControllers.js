325aab0569ae6683dc80d472cf606aca17b476aa
aldi278 <mohammad.mauliadi@gmail.com>
2021-10-19 21:02:57 +0700

    BE-Aldi-001 : Membuat API Add Parcel

--- controllers/parcelControllers.js
+++ controllers/parcelControllers.js
@@ -1,4 +1,6 @@
 const { db } = require("../database");
+const { uploader } = require("../helper/uploader");
+const fs = require("fs");
 
 module.exports = {
   getParcelsUser: (req, res) => {
@@ -35,6 +37,7 @@ module.exports = {
             categoryQuantity: [res.categories, res.quantities],
           }
         })
+        console.log(final)
         return res.status(200).send({
           success: true,
           data: final
@@ -64,5 +67,123 @@ module.exports = {
         data: "Missing query!"
       })
     }
-  }
+  },
+  addParcel: (req, res) => {
+    if (req.user.role === "admin") {
+        try {
+          let path = "/images";
+          const upload = uploader(path, "IMG").fields([{ name: "file" }]);
+
+          upload(req, res, (error) => {
+            if (error) {
+              console.log(error);
+              res.status(500).send(error);
+            }
+            const { file } = req.files;
+            const filepath = file ? path + "/" + file[0].filename : null;
+            let data = JSON.parse(req.body.data);
+            console.log(`data:`,data)
+            let {category, name, description, margin, price, image} = data
+            image = filepath;
+            
+            if(category && name && description && margin && price && image){
+              let addParcelQuery = `insert into parcels values (null, ${db.escape(
+                data.name
+              )}, ${db.escape(data.price)}, ${db.escape(data.margin)}, ${db.escape(
+                filepath
+              )}, ${db.escape(data.description)}, 'true')`;
+              db.query(addParcelQuery, (errParcelQuery, resultParcelQuery) => {
+                if (errParcelQuery) {
+                  fs.unlinkSync("./public" + filepath);
+                  return res.status(500).send({
+                    success: false,
+                    data: errParcelQuery,
+                  });
+                } else if (resultParcelQuery.insertId) {
+                  for(let i = 0;i<data.category.length;i++){
+                    let id_category = parseInt(data.category[i].category)
+                    let quantity = parseInt(data.category[i].category)
+                    let addCategoryQuery = `insert into parcel_categories values (null, ${db.escape(resultParcelQuery.insertId)}, ${db.escape(id_category)}, ${db.escape(quantity)})`
+                    console.log(`addCategoryQuery ke-${i} : ${addCategoryQuery}`)
+                    db.query(addCategoryQuery, (errCategoryQuery, resultCategoryQuery) => {
+                      if (errCategoryQuery) {
+                        fs.unlinkSync("./public" + filepath);
+                        return res.status(500).send({
+                          success: false,
+                          data: errCategoryQuery,
+                        });
+                      }
+                    });
+                  }
+                  
+                  let getQueryParcels = `select * from parcels where id_parcel='${resultParcelQuery.insertId}'`;
+                  db.query(getQueryParcels, (errGetParcel, resultGetParcel) => {
+                    if (errGetParcel) {
+                      fs.unlinkSync("./public" + filepath);
+                      return res.status(500).send({
+                        success: false,
+                        data: errGetParcel,
+                      });
+                    } else {
+                      return res.status(200).send({
+                        success: true,
+                        data: resultGetParcel[0],
+                      });
+                    }
+                  });
+                }
+              });
+            }else{
+              res.status(500).send({
+                success: false,
+                data: "Missing query!",
+              });
+            }
+          });
+        } catch (error) {
+          console.log(error);
+          res.status(500).send({
+            success: false,
+            data: error,
+          });
+        }
+    } else {
+      return res.status(500).send({
+        success: false,
+        data: "User not allowed!",
+      });
+    }
+  },
+  deleteParcel: (req, res) => {
+    if (req.user.role === "admin") {
+      if (req.query.id) {
+        let deleteQuery = `update parcels set active = 'false' where id_parcel = ${db.escape(
+          req.query.id
+        )}`;
+        db.query(deleteQuery, (errorDeleteQuery, resultDeleteQuery) => {
+          if (errorDeleteQuery) {
+            return res.status(500).send({
+              success: false,
+              data: errorDeleteQuery,
+            });
+          } else {
+            return res.status(200).send({
+              success: true,
+              data: "Delete succeed!",
+            });
+          }
+        });
+      } else {
+        return res.status(500).send({
+          success: false,
+          data: "Missing query!",
+        });
+      }
+    } else {
+      return res.status(500).send({
+        success: false,
+        data: "User not allowed!",
+      });
+    }
+  },
 }
--- routers/parcelRouters.js
+++ routers/parcelRouters.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const routers = express.Router();
 const { parcelControllers } = require("../controllers");
+const { auth } = require("../helper/authToken");
 
 routers.get("/get-parcels-user", parcelControllers.getParcelsUser);
 routers.get('/list', parcelControllers.listParcel)
 routers.get('/get', parcelControllers.getParcel)
+routers.post('/add', auth, parcelControllers.addParcel)
+routers.delete("/delete", auth, parcelControllers.deleteParcel)
 
 module.exports = routers;
