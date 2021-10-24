c7730947b5593e5d1ccc36fc1d72ad1bec0b9ae7
aldi278 <mohammad.mauliadi@gmail.com>
2021-10-24 09:59:14 +0700

    BE-Aldi-001 : Membuat API Get Transfer & Upload Bukti Transfer

--- routers/transactionRouters.js
+++ routers/transactionRouters.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const routers = express.Router();
 const { transactionControllers } = require("../controllers");
-const { auth } = require("../helper/authToken");
+const { auth, authToken } = require("../helper/authToken");
 
 routers.get("/income", auth, transactionControllers.incomeTransaction);
+routers.post("/get-all", authToken, transactionControllers.getAll);
+routers.get("/getTransactions", auth, transactionControllers.getUserTransactions);
+routers.patch("/uploadpayment", auth, transactionControllers.uploadImgTrf);
 
 module.exports = routers;
